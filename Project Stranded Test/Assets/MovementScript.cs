f6753afe781cb92f70ab7d34e251fce3492c13d4
Briken <brokenbriken@gmail.com>
2016-10-04 15:50:42 +0100

    Initial Commit
    initial commit with unit project folder

---
+++ Project Stranded Test/Assets/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+    Vector3 fleePoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Touch moveTouch = Input.GetTouch(0);
+
+        Debug.Log("Stop touching meeeee!");
+
+        fleePoint = moveTouch.position;
+    }
+
+    void MoveFromTouch(Vector3 targetPoint, Vector3 velocity)
+    {
+
+    }
+
+}
